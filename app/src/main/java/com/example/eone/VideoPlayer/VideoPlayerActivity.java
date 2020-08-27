7e9cda6d029bd208dabdaced8e2edc411a31c4be
admin <sumeshsarkar1997@gmail.com>
2020-08-27 15:45:10 +0530

    office2

--- app/src/main/java/com/example/eone/VideoPlayer/VideoPlayerActivity.java
+++ app/src/main/java/com/example/eone/VideoPlayer/VideoPlayerActivity.java
@@ -2,15 +2,61 @@ package com.example.eone.VideoPlayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.eone.R;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class VideoPlayerActivity extends AppCompatActivity {
+import java.security.Provider;
 
+public class VideoPlayerActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+    public static final String YOUTUBE_API_KEY = "AIzaSyDZSyRA_M4NtoBytqQN-CYsUe_aYjSJTCw";
+    private static final int RECOVERY_REQUEST = 1;
+    String videoId = null;
+
+    private YouTubePlayerView youTubeView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(YOUTUBE_API_KEY, this);
+        videoId = getIntent().getStringExtra("id");
     }
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo(videoId); // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), errorReason.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            youTubeView.initialize(YOUTUBE_API_KEY, this);
+        }
+    }
+
+
+
+
 }
